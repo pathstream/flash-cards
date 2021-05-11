1ae9a8d31014411702492881e6b6019bb9866dcf
Travis Jefferson <travis@pathstream.com>
2021-05-11 17:54:15 -0400

    Agenda!

--- src/FlashCards.js
+++ src/FlashCards.js
@@ -1,6 +1,17 @@
 import * as React from "react";
 import "./FlashCards.scss";
 
+/**
+ * TODO:
+ * - [] Q/A & intros
+ * - [] brainstorm requirements/goals/scope
+ * - [] start implementing FlashCards component
+ * - [] save some time for styling in FlashCards.scss (or elsewhere)
+ * - [] "take a break" and write a test or two
+ * - [] how hard to add some types?
+ * - [] what if we had lots more time?
+ * - [] some time for more questions at the end, possibly
+ */
 export function FlashCards() {
   return <div className="card-container">start building flash cards here!</div>;
 }
