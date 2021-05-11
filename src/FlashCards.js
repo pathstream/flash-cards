85a8f9b26b15b50ef368eb5194f869408279d059
Travis Jefferson <travis@pathstream.com>
2021-05-11 17:37:41 -0400

    Skeleton for flash cards

---
+++ src/FlashCards.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+import "./FlashCards.scss";
+
+export function FlashCards() {
+  return <div className="card-container">start building flash cards here!</div>;
+}
---
+++ src/FlashCards.test.js
@@ -0,0 +1,10 @@
+import userEvent from "@testing-library/user-event";
+import { screen, render } from "@testing-library/react";
+import { FlashCards } from "./FlashCards";
+
+test("should render something!", () => {
+  render(<FlashCards />);
+  const thing = screen.getByText("start building");
+  userEvent.click(thing);
+  expect(thing).toBeTruthy();
+});
