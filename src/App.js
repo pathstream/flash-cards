048bf50bf85e9200f649479a7ae0916758da5c7d
Travis Jefferson <travis@pathstream.com>
2021-08-10 11:50:44 -0400

    Scaffolding for typescript

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { FlashCards } from "./FlashCards";
+import { Welcome as WelcomeFromTypescript } from "./Example";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <WelcomeFromTypescript />
         <FlashCards />
       </header>
     </div>
---
+++ src/Example.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export function Welcome() {
+  return <div> Hello from typescript</div>;
+}
